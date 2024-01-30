eef89a2ab2796f0f06916c891b8f913a031afbf7
Dharuni Kannan <113665044+dharuni03@users.noreply.github.com>
2024-01-31 01:34:11 +0530

    Add files via upload

---
+++ day 5/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// import Login from './Components/Login.jsx'
+// import Register from './Components/Register.jsx'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    {/* <Login />
+    <Register /> */}
+     <App/> 
+  </React.StrictMode>,
+)
