e94f6c661113da6f08cf1a2daaba734d0d0d0709
Dharuni Kannan <113665044+dharuni03@users.noreply.github.com>
2024-02-09 11:35:54 +0530

    Delete day 4 directory

--- day 4/src/Components/About.jsx
+++
@@ -1,9 +0,0 @@
-import aboutus from '../assets/aboutus.png';
-const About = () => {
-    return (
-      <div>
-        <img src={aboutus} alt="about" style={{ width:'1260px', height: '60%', objectFit: 'cover', borderRadius:'8px'}}/>      </div>
-    );
-  };
-  
-  export default About;
